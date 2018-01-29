f3180b8d75725f16bff22ec4b895f1d45e0604cf
Agustin Falco <agustin.falco@x-team.com>
2018-01-29 09:21:41 -0300

    Initial commit.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import Header from './Header.jsx';
-import LoginPanel from './LoginPanel.jsx';
+import React, { Component } from 'react';
+import Editor from 'draft-js-plugins-editor'
+import createMarkdownShortcutsPlugin from 'draft-js-markdown-shortcuts-plugin';
+import ReactDOM from 'react-dom';
+import { EditorState} from 'draft-js';
 
-require('../styles/styles.scss');
 
-export default class App extends React.Component {
+const plugins = [
+  createMarkdownShortcutsPlugin()
+];
+
+export default class DemoEditor extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(editorState) {
+    this.setState({
+      editorState,
+    });
+  };
+
   render() {
-    let authorized = true;
-    if (authorized) {
-      return (
-          <LoginPanel> </LoginPanel>
-      );
-    } else {
-      return (
-          <Header/>
-      );
-    }
+    return (
+      <Editor
+        editorState={this.state.editorState}
+        onChange={this.onChange}
+        plugins={plugins}
+      />
+    );
   }
 }
